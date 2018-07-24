0cd8496528e32dc5dbac24644fbe73a3a8d09a79
John Grubbs <johnmlowery@gmail.com>
2018-07-24 17:59:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // write your code below!
-function happyHolodays() {
+function happyHolidays() {
   return "Happy Holidays!" }  
 function happyHolidaysTo(name) {
   return (`Happy Holidays, ${name}!`)
